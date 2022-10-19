2f6a392fba60c860af78c91f4df150a3acd7eef0
yosrgamal <yosr.elqott@gmail.com>
2022-10-19 17:30:30 +0200

    added file for constants

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//Color pallete
+const btColor = Color(0XFF588157);
+const tColor = Color(0XFF344E41);
+const icColor = Color(0XFF829577);
+
+const double defaultPadding = 20.0;
