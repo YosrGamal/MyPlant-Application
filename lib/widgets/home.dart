42904c167e79d4a43e20746a61306b92cb88a1a3
yosrgamal <yosr.elqott@gmail.com>
2022-10-19 13:40:11 +0200

    added routes

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -10,30 +10,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 200,
-            left: 10,
-            right: 10,
-            child: Container(
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        width: 3,
-                      ), //<-- SEE HERE
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    labelText: 'Search'),
-              ),
-              margin: const EdgeInsets.all(20.0),
-              height: 50,
-            ),
-          ),
-        ],
-      ),
+    return const Scaffold(
+      body: SafeArea(child: TextField()),
     );
   }
 }
