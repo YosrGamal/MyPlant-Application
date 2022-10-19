6d3edeb35cf614c5870991a15326f0633a13a1a3
yosrgamal <yosr.elqott@gmail.com>
2022-10-20 00:16:47 +0200

    edited imports

--- lib/widgets/routes.dart
+++ lib/widgets/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_plant_application/widgets/home.dart';
-import 'package:my_plant_application/widgets/login.dart';
+import 'package:my_plant_application/widgets/screens/home.dart';
+import 'package:my_plant_application/widgets/screens/login.dart';
 
 class Routes extends StatelessWidget {
   const Routes({super.key});
