b53cf02cb1642616d2f7615df684f1063acc8d3c
yosrgamal <yosr.elqott@gmail.com>
2022-10-20 00:16:31 +0200

    added themeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, duplicate_ignore, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:my_plant_application/constants.dart';
 import 'package:my_plant_application/widgets/routes.dart';
 
 void main() {
@@ -14,6 +15,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: bgcolor,
+        primaryColor: btColor,
+        primaryColorLight: icColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: tColor),
+      ),
       home: Routes(),
     );
   }
