e57032a423108b92ae4e5b9c93e462f28d9cfaa9
Dina ElRefaai <dina1911943@miuegypt.edu.eg>
2022-10-20 13:56:20 +0200

    adding bottoms of sign up

---
+++ lib/widgets/screens/signup.dart
@@ -0,0 +1,129 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+//
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+              child: Text('SignUp',
+                  style:
+                      TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+            )
+          ],
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: <Widget>[
+                // ignore: prefer_const_constructors, duplicate_ignore
+                TextField(
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color(0XFFD3D3D3),
+                      labelText: 'Name',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey))),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color(0XFFD3D3D3),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey))),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color(0XFFD3D3D3),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey))),
+                ),
+                SizedBox(height: 20.0),
+
+                Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      // ignore: deprecated_member_use
+                      primary: Color.fromARGB(255, 52, 78, 65),
+                      // ignore: deprecated_member_use
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: Text('SignUp'),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Center(
+                  child: InkWell(
+                    child: Text(
+                      'SignUp With Google Account?',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 52, 78, 65),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                          decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ),
+              ],
+            ))
+      ],
+    )));
+  }
+}
