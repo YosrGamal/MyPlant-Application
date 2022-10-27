4bc5e6191fe25aca2bb321ad3e520c27690fc123
Hagar-abdelfattah <102551441+Hagar-abdelfattah@users.noreply.github.com>
2022-10-27 16:52:24 +0200

    Update task.dart

--- lib/widgets/screens/task.dart
+++ lib/widgets/screens/task.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Reminder extends StatefulWidget {
+  @override
+  _ReminderState createState() => _ReminderState();
+}
+
+class _ReminderState extends State<Reminder> {
+  bool s1 = false, s2 = false, s3 = false, s4 = false, s5 = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+    
+    );
+  }
+}
 
