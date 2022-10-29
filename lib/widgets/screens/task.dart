7462c7ab687b629e1558a6736352a301e18ccaaf
Hagar-abdelfattah <hagar1911783@miuegypt.edu.eg>
2022-10-29 23:52:26 +0200

    update task

--- lib/widgets/screens/task.dart
+++ lib/widgets/screens/task.dart
@@ -3,14 +3,17 @@ import 'package:my_plant_application/widgets/components/body_task.dart';
 import 'package:my_plant_application/widgets/components/bottom_navbar.dart';
 
 class Task extends StatefulWidget {
+  const Task({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _TaskState createState() => _TaskState();
 }
 
 class _TaskState extends State<Task> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: BodyTask(),
       bottomNavigationBar: BottomNavBar(),
     );
