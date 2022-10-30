794052d70a17c4d70d0f5356c29e87c935ea1fef
Dinaa19 <dina_elrefaai@hotmail.com>
2022-10-30 23:47:35 +0200

    editing

--- lib/widgets/screens/signup.dart
+++ lib/widgets/screens/signup.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, duplicate_ignore
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -20,93 +21,6 @@ class _SignupState extends State<Signup> {
           children: <Widget>[
             Stack(
               children: <Widget>[
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter correct name';
-                    } else {
-                      return null;
-                    }
-                  },
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Color(0XFFD3D3D3),
-                      labelText: 'Name',
-                      labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey))),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    RegExp regex = RegExp(r'^(?=.*?[@])');
-                    if (value!.isEmpty) {
-                      return 'Please enter a email';
-                    } else {
-                      if (!regex.hasMatch(value)) {
-                        return 'Enter valid mail includes @';
-                      } else {
-                        return null;
-                      }
-                    }
-                  },
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Color(0XFFD3D3D3),
-                      labelText: 'Email',
-                      labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey))),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  validator: (value) {
-                    RegExp regex = RegExp(
-                        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~])');
-                    if (value!.isEmpty) {
-                      return 'Please enter a password';
-                    } else {
-                      if (!regex.hasMatch(value)) {
-                        return 'Please enter valid password';
-                      } else {
-                        return null;
-                      }
-                    }
-                  },
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Color(0XFFD3D3D3),
-                      labelText: 'Password',
-                      labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey))),
-                ),
                 SizedBox(height: 20.0),
                 const SizedBox(height: 150),
                 Image.asset('assets/images/app_logo.png',
@@ -116,7 +30,7 @@ class _SignupState extends State<Signup> {
                   child: Text('SignUp',
                       style: TextStyle(
                           fontSize: 30.0, fontWeight: FontWeight.normal)),
-                )
+                ),
               ],
             ),
             Container(
@@ -152,10 +66,15 @@ class _SignupState extends State<Signup> {
                     ),
                     TextFormField(
                       validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter an email';
+                        RegExp regex = RegExp(r'^(?=.*?[@])');
+                        if (value!.isEmpty) {
+                          return 'Please enter a email';
                         } else {
-                          return null;
+                          if (!regex.hasMatch(value)) {
+                            return 'Enter valid mail includes @';
+                          } else {
+                            return null;
+                          }
                         }
                       },
                       decoration: InputDecoration(
@@ -213,10 +132,9 @@ class _SignupState extends State<Signup> {
                       height: 60,
                       child: ElevatedButton(
                         onPressed: () {
-                          Navigator.pushNamed(
-                            context,
-                            '/',
-                          );
+                          if (formKey.currentState!.validate()) {
+                            context.go('/');
+                          }
                         },
                         style: ElevatedButton.styleFrom(
                           // ignore: deprecated_member_use
