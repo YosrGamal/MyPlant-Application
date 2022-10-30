190820e2a767d07ea80e2df2d5d92c21f99aaa07
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-10-30 07:37:27 +0200

    changed fonts, colors and sizes

--- lib/widgets/screens/change_password.dart
+++ lib/widgets/screens/change_password.dart
@@ -13,7 +13,7 @@ class ChangePasswordScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(height: 15),
+              const SizedBox(height: 30), //the space before settings
               Align(
                 alignment: Alignment.topLeft,
                 child: InkWell(
@@ -21,13 +21,21 @@ class ChangePasswordScreen extends StatelessWidget {
                     onTap: () {},
                     child: const Text(
                       'Settings',
-                      style: TextStyle(color: Colors.green, fontSize: 20),
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 124, 161, 126),
+                          fontFamily: 'Inter',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
                     )),
               ),
-              SizedBox(height: 15),
+              const SizedBox(height: 35), //space before change password text
               const Text('Change Password',
-                  style: TextStyle(fontSize: 30, color: Colors.black)),
-              SizedBox(height: 15),
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Color.fromARGB(255, 52, 78, 65),
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.bold)),
+              const SizedBox(height: 35),
               const TextField(
                 decoration: InputDecoration(
                     suffixIcon: Icon(Icons.remove_red_eye),
@@ -36,8 +44,8 @@ class ChangePasswordScreen extends StatelessWidget {
                         borderRadius: BorderRadius.all(Radius.circular(10)),
                         borderSide: BorderSide(color: Colors.green, width: 5))),
               ),
-              SizedBox(height: 15),
-              TextField(
+              const SizedBox(height: 15),
+              const TextField(
                 decoration: InputDecoration(
                     suffixIcon: Icon(Icons.remove_red_eye),
                     hintText: 'New Password',
@@ -45,8 +53,8 @@ class ChangePasswordScreen extends StatelessWidget {
                         borderRadius: BorderRadius.all(Radius.circular(10)),
                         borderSide: BorderSide(color: Colors.green, width: 5))),
               ),
-              SizedBox(height: 15),
-              TextField(
+              const SizedBox(height: 15),
+              const TextField(
                 decoration: InputDecoration(
                     suffixIcon: Icon(Icons.remove_red_eye),
                     hintText: 'Confirm Password',
@@ -54,11 +62,48 @@ class ChangePasswordScreen extends StatelessWidget {
                         borderRadius: BorderRadius.all(Radius.circular(10)),
                         borderSide: BorderSide(color: Colors.green, width: 5))),
               ),
-              TextButton(
-                  style:
-                      TextButton.styleFrom(backgroundColor: Colors.greenAccent),
+              const SizedBox(height: 30),
+
+              Container(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                width: 140,
+                height: 50,
+                child: ElevatedButton(
                   onPressed: () {},
-                  child: Text('Submit', style: TextStyle(color: Colors.white)))
+                  style: ElevatedButton.styleFrom(
+                    // ignore: deprecated_member_use
+                    primary: Color(0XFF588157),
+                    // ignore: deprecated_member_use
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                  child: Text('Submit'),
+                ),
+              ),
+
+              // TextButton(
+              //     style:
+              //         TextButton.styleFrom(backgroundColor: Colors.greenAccent),
+              //     onPressed: () {},
+              //     child: const Text('Submit',
+              //         style: TextStyle(color: Colors.white))),
+
+              const SizedBox(height: 20), //the space before settings
+              Align(
+                alignment: Alignment.center,
+                child: InkWell(
+                    //instead of TextField
+                    onTap: () {},
+                    child: const Text(
+                      'Forgot your password?',
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 124, 161, 126),
+                          fontSize: 17,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.bold),
+                    )),
+              ),
             ],
           ),
         ),
