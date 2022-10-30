7bd88eec8e1cf42224815e95621c093627693cba
Dinaa19 <dina_elrefaai@hotmail.com>
2022-10-30 23:10:14 +0200

    editing validation

--- lib/widgets/screens/signup.dart
+++ lib/widgets/screens/signup.dart
@@ -62,10 +62,15 @@ class _SignupState extends State<Signup> {
                 ),
                 TextFormField(
                   validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter an email';
+                    RegExp regex = RegExp(r'^(?=.*?[@])');
+                    if (value!.isEmpty) {
+                      return 'Please enter a email';
                     } else {
-                      return null;
+                      if (!regex.hasMatch(value)) {
+                        return 'Enter valid mail includes @';
+                      } else {
+                        return null;
+                      }
                     }
                   },
                   decoration: InputDecoration(
