540527cb0258ba2e0b42091f6f1b25a5cf633cef
YosrGamal <yosr.elqott@gmail.com>
2022-11-04 16:53:35 +0200

    added gorouter routes in a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_plant_application/constants.dart';
-// import 'package:my_plant_application/widgets/components/navbar.dart';
-// import 'package:my_plant_application/widgets/routes.dart';
-import 'package:go_router/go_router.dart';
-import 'package:my_plant_application/widgets/components/per_navbar.dart';
-import 'package:my_plant_application/widgets/screens/login.dart';
-import 'package:my_plant_application/widgets/screens/signup.dart';
-import 'package:my_plant_application/widgets/screens/library.dart';
-import 'package:my_plant_application/widgets/screens/settings.dart';
-import 'package:my_plant_application/widgets/screens/task.dart';
-import 'package:my_plant_application/widgets/screens/edit_profile.dart';
-import 'package:my_plant_application/widgets/screens/change_password.dart';
-
-import 'data/userdata.dart';
+import 'package:my_plant_application/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _router,
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: bgColor,
@@ -37,58 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-final GoRouter _router = GoRouter(
-  routes: <GoRoute>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return Login();
-      },
-    ),
-    GoRoute(
-      path: '/home',
-      builder: (BuildContext context, GoRouterState state) {
-        return PerNavbar();
-      },
-    ),
-    GoRoute(
-      path: '/signup',
-      builder: (BuildContext context, GoRouterState state) {
-        return Signup();
-      },
-    ),
-    GoRoute(
-      path: '/library',
-      builder: (BuildContext context, GoRouterState state) {
-        return Library();
-      },
-    ),
-    GoRoute(
-      path: '/tasks',
-      builder: (BuildContext context, GoRouterState state) {
-        return Task();
-      },
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (BuildContext context, GoRouterState state) {
-        return Settings();
-      },
-    ),
-    GoRoute(
-      path: '/editprofile',
-      builder: (BuildContext context, GoRouterState state) {
-        return EditProfile();
-      },
-    ),
-    GoRoute(
-      path: '/changepassword',
-      builder: (BuildContext context, GoRouterState state) {
-        return ChangePasswordScreen(
-          user: users[0],
-        );
-      },
-    ),
-  ],
-);
