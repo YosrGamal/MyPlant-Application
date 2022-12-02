531f769ab07456aaa3ff0a3f646d6804c28eeeea
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-12-03 00:28:08 +0200

    removing unused imports

--- lib/widgets/screens/reminder.dart
+++ lib/widgets/screens/reminder.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_plant_application/model/plantlogic.dart';
-import 'package:my_plant_application/widgets/components/body_reminder.dart';
 
 class Reminder extends StatelessWidget {
   const Reminder({super.key});
