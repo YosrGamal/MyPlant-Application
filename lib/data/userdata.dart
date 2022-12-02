62993c5e4cbdef5634f7b42e42f894cc4eb04639
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-12-03 00:31:22 +0200

    refactoring

--- lib/data/userdata.dart
+++ lib/data/userdata.dart
@@ -1,31 +1,31 @@
-import 'package:my_plant_application/model/userlogic.dart';
+import 'package:my_plant_application/model/user.dart';
 
-List<UserModel> users = [
-  UserModel(
+List<User> users = [
+  User(
       id: 1,
       email: 'Dina@gmail.com',
       username: 'Dina',
       password: 'H31@d',
       regDate: 'Jul, 2022'),
-  UserModel(
+  User(
       id: 2,
       email: 'Yosr@gmail.com',
       username: 'Yosr',
       password: 'Jk@9',
       regDate: 'Jun, 2022'),
-  UserModel(
+  User(
       id: 3,
       email: 'Hagar@gmail.com',
       username: 'Hagar',
       password: 'shI#1',
       regDate: 'Jul, 2022'),
-  UserModel(
+  User(
       id: 4,
       email: 'Omar@gmail.com',
       username: 'Omar',
       password: 'hIk9@',
       regDate: 'Aug, 2022'),
-  UserModel(
+  User(
       id: 5,
       email: 'Sam@gmail.com',
       username: 'Sam',
