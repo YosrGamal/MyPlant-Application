c2aa078cb24e793b16c7128132595f3d061f182d
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-12-06 01:18:37 +0200

    after user signs up, he will be sent to home page.

--- lib/widgets/screens/signup.dart
+++ lib/widgets/screens/signup.dart
@@ -190,7 +190,7 @@ class _SignupState extends State<Signup> {
                             'email': EmailController.text,
                             'password': PasswordController.text,
                           });
-                          context.go('/home');
+                          context.go('/navbar');
                           // }
                         },
                         style: ElevatedButton.styleFrom(
