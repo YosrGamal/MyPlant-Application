dea6c897de48d4527e726e3a86ee321c49950512
YosrGamal <yosr.elqott@gmail.com>
2022-12-07 18:30:24 +0200

    start with startpage instead of login

--- lib/routes.dart
+++ lib/routes.dart
@@ -22,7 +22,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const Login();
+        // return const Login();
         return const StartPage();
       },
     ),
