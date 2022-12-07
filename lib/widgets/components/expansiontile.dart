e47a30d4ba6eb9d19e16967497cf4ebe9babd1ad
hagar mohamed <hagar1911783@miuegypt.edu.eg>
2022-12-07 19:13:15 +0200

    remove comments

--- lib/widgets/components/expansiontile.dart
+++ lib/widgets/components/expansiontile.dart
@@ -5,34 +5,12 @@ class Expansiontile extends StatefulWidget {
   State<Expansiontile> createState() => _ExpansiontileState();
 }
 class _ExpansiontileState extends State<Expansiontile> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: const <Widget>[
-//         ExpansionTile(
-//           title: Text('Water',  textAlign: TextAlign.center,
-//                           style: TextStyle(
-//                     color: Color.fromARGB(255, 83, 118, 94), fontSize: 22),
-//           ),
-//           //subtitle: Text('Trailing expansion arrow icon'),
-//           children: [ListTile(
-//             title: Text('Snake Plant Sansevieria 7:00 am',  
-            //  textAlign: TextAlign.center,
-            //                  style: TextStyle(
-            //         color: Color.fromARGB(255, 83, 118, 94)
-            //         ),
-//         ))],
-//         ),
-//       ],
-//     );
-//   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
        //padding: const EdgeInsets.all(20.0),
-        padding: const EdgeInsets.fromLTRB(5.0, 150.0, 0.0, 50.0),
+        padding: const EdgeInsets.fromLTRB(5.0, 50.0, 0.0, 0.0),
         child: Column(
           children: const [
           ExpansionTile(
@@ -84,5 +62,4 @@ class _ExpansiontileState extends State<Expansiontile> {
       ),
     );
   }
-
 }
