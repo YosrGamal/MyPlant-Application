dc4ac91e84a48caecbfb7bc3cb39b56b75ab19ba
Dinaa19 <dina_elrefaai@hotmail.com>
2022-12-07 16:56:14 +0200

    deleting things

--- lib/widgets/screens/signup.dart
+++ lib/widgets/screens/signup.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, duplicate_ignore, unused_import
+// ignore_for_file: prefer_const_constructors, duplicate_ignore, unused_import, non_constant_identifier_names
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
