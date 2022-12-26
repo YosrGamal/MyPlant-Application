b89013075b841a8ef686d69bbf1e1e6caee7c257
Dinaa19 <dina_elrefaai@hotmail.com>
2022-12-26 13:26:36 +0200

    editing signin signup

---
+++ lib/view/signinsignup_buttoms.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SignInSignUpButton extends StatelessWidget {
+  const SignInSignUpButton(
+      {Key? key, required this.onTap, required this.isLogin})
+      : super(key: key);
+  final Function onTap;
+  final bool isLogin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+      child: ElevatedButton(
+        onPressed: () {
+          onTap();
+        },
+        // ignore: sort_child_properties_last
+        child: Text(
+          isLogin ? 'LOG IN' : 'SIGN UP',
+          style: const TextStyle(
+              color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.black26;
+              }
+              return Colors.white;
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)))),
+      ),
+    );
+  }
+}
