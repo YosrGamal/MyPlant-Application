e793afc68e714fdcc58366cb21eaa07ca0507f6e
hagar mohamed <hagar1911783@miuegypt.edu.eg>
2022-12-28 13:19:46 +0200

    add icon

--- lib/view/Users/widgets/components/body_setting_reminder.dart
+++ lib/view/Users/widgets/components/body_setting_reminder.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:my_plant_application/view/Users/widgets/components/setting_reminder_header.dart';
+import 'package:my_plant_application/view/Users/widgets/components/switch_setting_reminder.dart';
 
 class Settingreminder extends StatelessWidget {
   const Settingreminder({super.key});
   @override
   Widget build(BuildContext context) {
+     const Switchsettingreminder();
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
@@ -15,6 +17,28 @@ class Settingreminder extends StatelessWidget {
               height: MediaQuery.of(context).size.height - 80,
               width: double.infinity,
             ),
+            Container(
+            margin: const EdgeInsets.only(top: 302),
+            padding: const EdgeInsets.all(60),
+            height: 30,
+            width: 70,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/icons/evening.png"),
+                  fit: BoxFit.contain),
+            ),
+          ),
+            Container(
+            margin: const EdgeInsets.only(top: 250),
+            padding: const EdgeInsets.all(60),
+            height: 30,
+            width: 70,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/icons/sun.png"),
+                  fit: BoxFit.contain),
+            ),
+          ),
             HeaderSettingReminder(size: size),
           ]),
         ],
