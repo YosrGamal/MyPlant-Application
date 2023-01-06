d157a7a73a401cd3aa8e95c66d702447fc6a192a
YosrGamal <yosr.elqott@gmail.com>
2023-01-06 22:27:05 +0200

    commented

--- lib/data/db_helper.dart
+++ lib/data/db_helper.dart
@@ -1,31 +1,31 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+// import 'package:sqflite/sqflite.dart';
+// import 'package:path/path.dart';
 
-class DBHelper {
-  static Database? _db;
-  Future<Database?> get db async {
-    if (_db == null) {
-      _db = await initialDB();
-      return _db;
-    } else {
-      return _db;
-    }
-  }
+// class DBHelper {
+//   static Database? _db;
+//   Future<Database?> get db async {
+//     if (_db == null) {
+//       _db = await initialDB();
+//       return _db;
+//     } else {
+//       return _db;
+//     }
+//   }
 
-  initialDB() async {
-    String databasepath = await getDatabasesPath();
-    String path = join(databasepath, 'myplant.db');
-    Database mydb = await openDatabase(path, onCreate: _onCreate);
-    return mydb;
-  }
+//   initialDB() async {
+//     String databasepath = await getDatabasesPath();
+//     String path = join(databasepath, 'myplant.db');
+//     Database mydb = await openDatabase(path, onCreate: _onCreate);
+//     return mydb;
+//   }
 
-  _onCreate(Database db, int version) async {
-    await db.execute('''
-    CREATE TABLE plants(
-      id TEXT NOT NULL PRIMARY KEY 
-      name TEXT
-      imageurl TEXT
-      isfav BOOL
-    ) ''');
-  }
-}
+//   _onCreate(Database db, int version) async {
+//     await db.execute('''
+//     CREATE TABLE plants(
+//       id TEXT NOT NULL PRIMARY KEY 
+//       name TEXT
+//       imageurl TEXT
+//       isfav BOOL
+//     ) ''');
+//   }
+// }
