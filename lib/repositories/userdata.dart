fafe4f459d33cf8d0b2b3fb16126f5f2e01ffbec
YosrGamal <yosr.elqott@gmail.com>
2023-01-08 14:19:57 +0200

    added dob and profile pic

--- lib/repositories/userdata.dart
+++ lib/repositories/userdata.dart
@@ -2,33 +2,35 @@ import 'package:my_plant_application/domain/user.dart';
 
 List<User> users = [
   User(
-      id: 1,
+      id: '1',
       email: 'Dina@gmail.com',
       username: 'Dina',
       password: 'H31@d',
+      dob: '02/2/2001',
+      profilepic: '.....',
       regDate: 'Jul, 2022'),
   User(
-      id: 2,
+      id: '2',
       email: 'Yosr@gmail.com',
       username: 'Yosr',
       password: 'Jk@9',
+      dob: '02/2/2001',
+      profilepic: '.....',
       regDate: 'Jun, 2022'),
   User(
-      id: 3,
+      id: '3',
       email: 'Hagar@gmail.com',
       username: 'Hagar',
       password: 'shI#1',
+      dob: '02/2/2001',
+      profilepic: '.....',
       regDate: 'Jul, 2022'),
   User(
-      id: 4,
+      id: '4',
       email: 'Omar@gmail.com',
       username: 'Omar',
       password: 'hIk9@',
+      dob: '02/2/2001',
+      profilepic: '.....',
       regDate: 'Aug, 2022'),
-  User(
-      id: 5,
-      email: 'Sam@gmail.com',
-      username: 'Sam',
-      password: 'Lmn9#',
-      regDate: 'Sept, 2022')
 ];
