0ce2dab9ff8e511a2f801da85e505abb65cf0b89
YosrGamal <yosr.elqott@gmail.com>
2023-01-17 01:18:26 +0200

    added camera

--- lib/view/Users/widgets/screens/camera.dart
+++ lib/view/Users/widgets/screens/camera.dart
@@ -16,7 +16,6 @@ class Camera extends StatefulWidget {
 class _CameraState extends State<Camera> {
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // ChooseImage();
+    return ChooseImage();
   }
 }
