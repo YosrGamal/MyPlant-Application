5a8412cca257a9eb97a556d45c3168cb98bb8454
YosrGamal <yosr.elqott@gmail.com>
2023-01-18 17:02:31 +0200

    made some edits

--- lib/view/Users/widgets/tfmodel/chooseimage.dart
+++ lib/view/Users/widgets/tfmodel/chooseimage.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: use_build_context_synchronously, avoid_print, must_be_immutable, unused_local_variable, duplicate_ignore
-
 import 'dart:io';
-
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,51 +7,35 @@ import 'package:image_picker/image_picker.dart';
 
 import 'package:my_plant_application/constants.dart';
 // ignore: depend_on_referenced_packages
-import 'package:firebase_storage/firebase_storage.dart';
 // import 'package:tflite/tflite.dart';
 // import 'package:my_plant_application/view/Users/widgets/tfmodel/loadingscreen.dart';
 // import 'package:my_plant_application/view/Users/widgets/tfmodel/showingresult.dart';
 
 // ignore: must_be_immutable
 class ChooseImage extends StatefulWidget {
-  String? imageurl;
-  ChooseImage({this.imageurl, super.key});
+  const ChooseImage({super.key});
 
   @override
   State<ChooseImage> createState() => _ChooseImageState();
 }
 
 class _ChooseImageState extends State<ChooseImage> {
-  XFile? image;
+  File? image;
+  Future<String>? imageurl;
 
-  String imageUrl = '';
-
-  Future uploadImage() async {
-    // final path = '';
-    // final file = File(image!.path);
-
-    File? file = await pickImage(ImageSource.gallery);
-
-    if (file == null) return;
-    String uniqueFileName = DateTime.now().millisecondsSinceEpoch.toString();
-
-    Reference referenceRoot = FirebaseStorage.instance.ref();
-    Reference referenceDirImages = referenceRoot.child('images');
-    Reference referenceImageToUpload = referenceDirImages.child(uniqueFileName);
-
-    try {
-      await referenceImageToUpload.putFile(File(file.path));
-
-      ScaffoldMessenger.of(context)
-          .showSnackBar(const SnackBar(content: Text('success')));
-
-      imageUrl = await referenceImageToUpload.getDownloadURL();
-
-      print(imageUrl);
-    } catch (error) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(error.toString())));
-    }
+  Future<String> uploadImage() async {
+    // File? file = await pickImage(ImageSource.gallery);
+    Future<String>? url;
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference ref = storage.ref().child("images" + DateTime.now().toString());
+    UploadTask uploadTask = ref.putFile(image!);
+    uploadTask.whenComplete(() {
+      url = ref.getDownloadURL();
+    }).catchError((onError) {
+      print(onError);
+    });
+    print(url);
+    return url!;
   }
 
   Future pickImage(source) async {
@@ -61,7 +43,7 @@ class _ChooseImageState extends State<ChooseImage> {
       final img = await ImagePicker().pickImage(source: source);
       if (img == null) return null;
 
-      final imageTemporary = XFile(img.path);
+      final imageTemporary = File(img.path);
       setState(() {
         // Loading(loading: true);
         image = imageTemporary;
@@ -101,7 +83,8 @@ class _ChooseImageState extends State<ChooseImage> {
               ),
               child: const Text('Pick From Gallery',
                   style: TextStyle(fontSize: 20, fontFamily: 'Inter')),
-              onPressed: () async {
+              onPressed: () {
+                pickImage(ImageSource.gallery);
                 uploadImage();
               }),
           ElevatedButton(
