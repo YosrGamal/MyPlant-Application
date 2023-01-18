d69ecd17c96dda67dbb2365f19dde45e08a332d0
YosrGamal <yosr.elqott@gmail.com>
2023-01-18 19:19:37 +0200

    made changes

--- lib/data/repository/plant_rep.dart
+++ lib/data/repository/plant_rep.dart
@@ -1,28 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-Future createPlant({
-  required String name,
-  required String image,
-  required bool isfav,
-}) async {
-  final plantInstance = FirebaseFirestore.instance.collection('plants').doc();
+class PlantRep {
+  Future createPlant({
+    required String name,
+    required String image,
+    required bool isfav,
+  }) async {
+    final plantInstance = FirebaseFirestore.instance.collection('plants').doc();
 
-  final plantData = {
-    'name': name,
-    'image': image,
-    'isfav': isfav,
-  };
+    final plantData = {
+      'name': name,
+      'image': image,
+      'isfav': isfav,
+    };
 
-  await plantInstance.set(plantData);
-}
+    await plantInstance.set(plantData);
+  }
 
-Future fetchAllPlants() async {
-  FirebaseFirestore.instance.collection('plants').get();
-}
+  Future fetchAllPlants() async {
+    FirebaseFirestore.instance.collection('plants').get();
+  }
 
-Future removePlant(String id) async {
-  DocumentReference documentReference =
-      FirebaseFirestore.instance.collection('plants').doc(id);
+  Future removePlant(String id) async {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('plants').doc(id);
 
-  await documentReference.delete();
+    await documentReference.delete();
+  }
 }
