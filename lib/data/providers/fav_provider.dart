61f8945a75a3450dfe96c415e384077045630b64
YosrGamal <yosr.elqott@gmail.com>
2023-01-18 19:21:37 +0200

    commented

--- lib/data/providers/fav_provider.dart
+++ lib/data/providers/fav_provider.dart
@@ -1,28 +1,28 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Fav extends StateNotifier<List<String>> {
-  Fav() : super([]);
+// class Fav extends StateNotifier<List<String>> {
+//   Fav() : super([]);
 
-  void addItem(String id) {
-    state = [...state, id];
-  }
+//   void addItem(String id) {
+//     state = [...state, id];
+//   }
 
-  void removeItem(String id) {
-    state = [
-      for (final i in state)
-        if (i != id) id
-    ];
-  }
+//   void removeItem(String id) {
+//     state = [
+//       for (final i in state)
+//         if (i != id) id
+//     ];
+//   }
 
-  bool isFav(String id) {
-    var value = state.firstWhere((i) => i == id, orElse: () => "-2");
-    if (value == "-2") {
-      return false;
-    }
-    return true;
-  }
-}
+//   bool isFav(String id) {
+//     var value = state.firstWhere((i) => i == id, orElse: () => "-2");
+//     if (value == "-2") {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
 
-final favProvider = StateNotifierProvider<Fav, List<String>>((ref) {
-  return Fav();
-});
+// final favProvider = StateNotifierProvider<Fav, List<String>>((ref) {
+//   return Fav();
+// });
