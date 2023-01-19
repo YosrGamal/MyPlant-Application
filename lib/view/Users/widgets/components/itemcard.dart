78db65977f00744989b50b13e5c00cceb95ddd2f
hagar mohamed <hagar1911783@miuegypt.edu.eg>
2023-01-19 10:30:19 +0200

    made change

--- lib/view/Users/widgets/components/itemcard.dart
+++ lib/view/Users/widgets/components/itemcard.dart
@@ -15,7 +15,7 @@ class ItemCard extends ConsumerStatefulWidget {
 }
 
 class _ItemCardState extends ConsumerState<ItemCard> {
-  @override
+ @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () =>
@@ -29,72 +29,116 @@ class _ItemCardState extends ConsumerState<ItemCard> {
             ),
             borderRadius: BorderRadius.all(Radius.circular(12)),
           ),
-          child: Column(children: [
-            Stack(children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 5),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: AspectRatio(
-                      aspectRatio: 16 / 16,
-                      child: Image.network(
-                        widget.plant.imageUrl!,
-                        fit: BoxFit.cover,
-                      ),
-                    )),
-              ),
-              Consumer(builder: (context, WidgetRef ref, __) {
-                ref.watch(favProvider);
-                bool isfav =
-                    ref.watch(favProvider.notifier).isFav(widget.plant.id!);
-
-                return Padding(
+          child: Column(
+            children: [
+              Stack(children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: AspectRatio(
+                        aspectRatio: 16 / 16,
+                        child: Image.network(
+                          widget.plant.imageUrl!,
+                          fit: BoxFit.cover,
+                        ),
+                      )),
+                ),
+                Padding(
                   padding: const EdgeInsets.all(2.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      IconButton(
-                        splashColor: Colors.blueAccent,
-                        icon: isfav
-                            ? const Icon(Icons.favorite)
-                            : const Icon(Icons.favorite_border_outlined),
-                        color: const Color.fromARGB(255, 185, 39, 28),
-                        onPressed: () {
-                          if (isfav) {
-                            isfav = false;
-                          } else {
-                            isfav = true;
-                          }
-                          if (isfav) {
-                            ref
-                                .read(favProvider.notifier)
-                                .addItem(widget.plant.id!);
-                          } else {
-                            ref
-                                .read(favProvider.notifier)
-                                .removeItem(widget.plant.id!);
-                          }
-                          // provider.toggleFavorite(PlantModel(
-                          //     id: widget.plant.id,
-                          //     name: widget.plant.name,
-                          //     imageUrl: widget.plant.imageUrl,
-                          //     isfavorite: widget.plant.isfavorite));
-                        },
-                      ),
-                      // FavoriteButton(
-                      //   isFavorite: widget.plant.isfavorite,
-                      //   iconSize: 40,
-                      //   iconColor: btColor,
-                      //   valueChanged: (isFavorite) {
-                      //     widget.plant.isfavorite;
+                      // IconButton(
+                      //   splashColor: Colors.blueAccent,
+                      //   icon: provider.isExist(PlantModel(
+                      //           id: widget.plant.id,
+                      //           name: widget.plant.name,
+                      //           imageUrl: widget.plant.imageUrl,
+                      //           isfavorite: widget.plant.isfavorite))
+                      //       ? const Icon(Icons.favorite)
+                      //       : const Icon(Icons.favorite_border_outlined),
+                      //   color: Color.fromARGB(255, 185, 39, 28),
+                      //   onPressed: () {
+                      //     provider.toggleFavorite(PlantModel(
+                      //         id: widget.plant.id,
+                      //         name: widget.plant.name,
+                      //         imageUrl: widget.plant.imageUrl,
+                      //         isfavorite: widget.plant.isfavorite));
                       //   },
                       // ),
+                      FavoriteButton(
+                        isFavorite: widget.plant.isfavorite,
+                        iconSize: 40,
+                        iconColor: btColor,
+                        valueChanged: (isFavorite) {
+                          widget.plant.isfavorite;
+                        },
+                      ),
                     ],
                   ),
-                );
-              }),
-            ]),
-          ]),
+                ),
+                Consumer(builder: (context, WidgetRef ref, __) {
+                  ref.watch(favProvider);
+                  bool isfav =
+                      ref.watch(favProvider.notifier).isFav(widget.plant.id!);
+
+                  return Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          splashColor: Colors.blueAccent,
+                          icon: isfav
+                              ? const Icon(Icons.favorite)
+                              : const Icon(Icons.favorite_border_outlined),
+                          color: const Color.fromARGB(255, 185, 39, 28),
+                          onPressed: () {
+                            if (isfav) {
+                              isfav = false;
+                            } else {
+                              isfav = true;
+                            }
+                            if (isfav) {
+                              ref
+                                  .read(favProvider.notifier)
+                                  .addItem(widget.plant.id!);
+                            } else {
+                              ref
+                                  .read(favProvider.notifier)
+                                  .removeItem(widget.plant.id!);
+                            }
+                            // provider.toggleFavorite(PlantModel(
+                            //     id: widget.plant.id,
+                            //     name: widget.plant.name,
+                            //     imageUrl: widget.plant.imageUrl,
+                            //     isfavorite: widget.plant.isfavorite));
+                          },
+                        ),
+                        // FavoriteButton(
+                        //   isFavorite: widget.plant.isfavorite,
+                        //   iconSize: 40,
+                        //   iconColor: btColor,
+                        //   valueChanged: (isFavorite) {
+                        //     widget.plant.isfavorite;
+                        //   },
+                        // ),
+                      ],
+                    ),
+                  );
+                }),
+              ]),
+              Padding(
+                padding: const EdgeInsets.all(0),
+                child: Text(
+                  widget.plant.name!,
+                  style: const TextStyle(
+                      color: tColor, fontFamily: 'Inter', fontSize: 14),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
