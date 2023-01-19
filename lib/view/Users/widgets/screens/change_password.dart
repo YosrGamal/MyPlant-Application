5c4c2cfe0405241beae8c6ac279ada0a63469ac7
OmegaElrefaie <omegaelrefaie@gmail.com>
2023-01-19 07:21:25 +0200

    unused funciton

--- lib/view/Users/widgets/screens/change_password.dart
+++ lib/view/Users/widgets/screens/change_password.dart
@@ -21,16 +21,3 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
     );
   }
 }
-
-void _changePassword(String password) async {
-  //Create an instance of the current user.
-  var user = await FirebaseAuth.instance.currentUser!;
-
-  //Pass in the password to updatePassword.
-  user.updatePassword(password).then((_) {
-    print("Successfully changed password");
-  }).catchError((error) {
-    print("Password can't be changed" + error.toString());
-    //This might happen, when the wrong password is in, the user isn't found, or if the user hasn't logged in recently.
-  });
-}
